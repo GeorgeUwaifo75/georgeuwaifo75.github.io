ee0a4331036e8d9a94e7331cb438e037f3104af9
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-12-17 16:44:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 const CONFIG = {
     API_KEY: process.env.JSONBIN_API_KEY || localStorage.getItem('jsonbin_api_key')
 };
+
+const CONFIG2 = {
+    API_KEY: process.env.JSONBIN_MB_VALUE || localStorage.getItem('jsonbin_mb_value')
+};
