b24ee0bc60bf9aca300051dbe117f4ceef194251
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2026-01-13 15:03:35 +0100

    Update api.js

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ class JSONBinAPI {
       // Replace with your actual JSONBin.io credentials
       this.apiKey = '$2a$10$GY26W.StiN7bdlaoYuva3.GCGhyglj8ne8v0aaIJ895NLv9o61bqy'; 
       // Your JSONBin.io API key
-      this.mainBinId = JSONBIN_MAIN_BIN_ID;
+      this.mainBinId = '693b1ac443b1c97be9e786b2';
        // Your JSONBin.io main bin ID for users
        this.baseURL = 'https://api.jsonbin.io/v3/b';
    
