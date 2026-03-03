74d866c5ff98280eea359fc206ef325c759d368f
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2026-03-03 15:28:27 +0100

    Create config.js

---
+++ giteksolhub/config.js
@@ -0,0 +1,26 @@
+// config.js
+const CONFIG = {
+    //JSONBIN_API_KEY: '$2a$10$GY26W.StiN7bdlaoYuva3.GCGhyglj8ne8v0aaIJ895NLv9o61bqy',
+     JSONBIN_API_KEY: '$2a$10$p33UKD.Ju51IwJKELeOTKerEQM8JPavlsiRQ1ANegN7b/2bkEMvku',
+    
+    JSONBIN_MAIN_BIN_ID: '69989ac443b1c97be98eea38',
+    PAYSTACK_PUBLIC_KEY: 'pk_live_2018244c913523ab0751249b240bc3e3448c3c19',
+    BINS: {
+        ALLUSERS: 'allusers',
+        ALLPRODUCTS: 'allproducts',
+        ALLPAYMENTS: 'allpayments'
+    }
+};
+
+const CATEGORIES = [
+    'Supermarkets',
+    'Computing and Electronics',
+    'Computer Services',
+    'Household Products',
+    'Wholesale food commodities',
+    'Printing and Publishing',
+    'Automobiles',
+    'Food services',
+    'Rentals and Properties',
+    'Furniture and others'
+];
