197504bbcd42a3bd8ef8c2d49affa9a58efe3c47
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2026-03-08 21:01:20 +0000

    Update config.js

--- giteksolhub/config.js
+++ giteksolhub/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
-    JSONBIN_API_KEY: '$2a$10$GY26W.StiN7bdlaoYuva3.GCGhyglj8ne8v0aaIJ895NLv9o61bqy',
-    //JSONBIN_API_KEY: '$2a$10$p33UKD.Ju51IwJKELeOTKerEQM8JPavlsiRQ1ANegN7b/2bkEMvku',
+    //JSONBIN_API_KEY: '$2a$10$GY26W.StiN7bdlaoYuva3.GCGhyglj8ne8v0aaIJ895NLv9o61bqy',
+    JSONBIN_API_KEY: '$2a$10$p33UKD.Ju51IwJKELeOTKerEQM8JPavlsiRQ1ANegN7b/2bkEMvku',
     JSONBIN_MAIN_BIN_ID: '69a99281d0ea881f40f1dc8d', // Your new bin ID
     PAYSTACK_PUBLIC_KEY: 'pk_live_2018244c913523ab0751249b240bc3e3448c3c19',
     BINS: {
